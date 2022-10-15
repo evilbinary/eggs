0f95d2873fd4b126cc9d0ff16ef9705fdb65b928
evilbinary <rootntsd@gmail.com>
2022-10-15 21:19:19 +0800

    ref libmusl

--- libmusl/crt/crt1.c
+++ libmusl/crt/crt1.c
@@ -3,17 +3,22 @@
 
 #define START "_start"
 
-#include "crt_arch.h"
+typedef struct exec {
+  int argc;
+  char **argv;
+  char **envp;
+  char *filename;
+} exec_t;
+
+// #include "crt_arch.h"
 
 int main();
 weak void _init();
 weak void _fini();
-int __libc_start_main(int (*)(), int, char **,
-	void (*)(), void(*)(), void(*)());
+int __libc_start_main(int (*)(), int, char **);
 
-void _start_c(long *p)
-{
-	int argc = p[0];
-	char **argv = (void *)(p+1);
-	__libc_start_main(main, argc, argv, _init, _fini, 0);
+void _start_c(exec_t *p) {
+  int argc = p->argc;
+  char **argv = (void *)&(p->argv);
+  __libc_start_main(main, argc, argv);
 }
