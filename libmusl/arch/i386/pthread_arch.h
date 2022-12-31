e3d98d18ee41be874c0e135ce07f50f2e114ba08
evilbinary <rootntsd@gmail.com>
2023-01-01 01:49:39 +0800

    fix get tp

--- libmusl/arch/i386/pthread_arch.h
+++ libmusl/arch/i386/pthread_arch.h
@@ -1,9 +1,11 @@
+#define SYS_THREAD_SELF 414
+
 static inline uintptr_t __get_tp()
 {
 	uintptr_t tp;
 	//todo
 	// __asm__ ("movl %%gs:0,%0" : "=r" (tp) );
-	tp= __syscall(SYS_getpid);
+	tp= __syscall(SYS_THREAD_SELF);
 	return tp;
 }
 
