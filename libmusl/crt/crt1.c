cce37bd86e67baefbbe2b5328c9384e0f2b6ffca
evilbinary <rootntsd@gmail.com>
2022-12-31 19:13:36 +0800

    fix argv

--- libmusl/crt/crt1.c
+++ libmusl/crt/crt1.c
@@ -15,10 +15,11 @@ typedef struct exec {
 int main();
 weak void _init();
 weak void _fini();
-int __libc_start_main(int (*)(), int, char **);
-
+int __libc_start_main(int (*)(), int, char **,
+	void (*)(), void(*)(), void(*)());
 void _start_c(exec_t *p) {
+
   int argc = p->argc;
-  char **argv = (void *)&(p->argv);
-  __libc_start_main(main, argc, argv);
+	char **argv = p->argv;
+  __libc_start_main(main, argc, argv, _init, _fini, 0);
 }
