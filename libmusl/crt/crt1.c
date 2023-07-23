656cc5f0c8ebc02b3de1fd3fd2d5d8cebf03e2d7
evilbinary <rootntsd@gmail.com>
2023-07-23 23:36:34 +0800

    ref crt

--- libmusl/crt/crt1.c
+++ libmusl/crt/crt1.c
@@ -1,7 +1,10 @@
 #include <features.h>
+
 #include "libc.h"
 
 #define START "_start"
+
+#ifdef USE_DUCK_ENTRY
 
 typedef struct exec {
   int argc;
@@ -15,18 +18,34 @@ typedef struct exec {
 int main();
 weak void _init();
 weak void _fini();
-int __libc_start_main(int (*)(), int, char **,
-	void (*)(), void(*)(), void(*)());
-
+int __libc_start_main(int (*)(), int, char **, void (*)(), void (*)(),
+                      void (*)());
 
 void start(exec_t *p) {
   int argc = p->argc;
-	char **argv = p->argv;
+  char **argv = p->argv;
   __libc_start_main(main, argc, argv, _init, _fini, 0);
 }
 
 void _start_c(exec_t *p) {
   int argc = p->argc;
-	char **argv = p->argv;
+  char **argv = p->argv;
   __libc_start_main(main, argc, argv, _init, _fini, 0);
 }
+
+#else
+#include "crt_arch.h"
+
+int main();
+weak void _init();
+weak void _fini();
+int __libc_start_main(int (*)(), int, char **, void (*)(), void (*)(),
+                      void (*)());
+
+void _start_c(long *p) {
+  int argc = p[0];
+  char **argv = (void *)(p + 1);
+  __libc_start_main(main, argc, argv, _init, _fini, 0);
+}
+
+#endif
