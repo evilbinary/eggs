4ab962683d41b3ca070ae06f82287b37f86ab14b
evilbinary <rootntsd@gmail.com>
2023-11-01 22:43:10 +0800

    add ya

---
+++ liblvgl-8.0.0/ya.py
@@ -0,0 +1,35 @@
+target("lvgl-8.0.0")
+set_kind("static")
+
+add_packages("musl")
+add_deps("gui")
+
+add_files(
+    # 'lv_conf.h',
+    'port/lv_port_disp.c',
+    # "port/lv_port_fs.c',",
+    'port/mouse_cursor_icon.c',
+    'port/lv_port_indev.c'
+)
+
+add_files(
+    'src/core/*.c',
+    'src/draw/*.c',
+    'src/font/*.c',
+    'src/gpu/*.c',
+    'src/hal/*.c',
+    'src/misc/*.c',
+    'src/widgets/*.c',
+    'src/extra/*.c',
+    'src/extra/**/*.c',
+    'src/extra/**/**/*.c'
+)
+
+add_cflags('-DLV_CONF_INCLUDE_SIMPLE ')
+
+
+add_includedirs(
+    '.',
+    '../include',
+    public = true
+)
---
+++ libpng-1.6.37/ya.py
@@ -0,0 +1,28 @@
+target("png")
+set_kind("static")
+
+add_packages("musl")
+add_deps("zlib")
+
+add_files(
+    "png.c",
+    "pngerror.c",
+    "pngget.c",
+    "pngmem.c",
+    "pngpread.c",
+    "pngread.c",
+    "pngrio.c",
+    "pngrtran.c",
+    "pngrutil.c",
+    "pngset.c",
+    "pngtrans.c",
+    "pngwio.c",
+    "pngwrite.c",
+    "pngwtran.c",
+    "pngwutil.c"
+)
+
+add_includedirs(
+    '.',
+    '../include'
+)
