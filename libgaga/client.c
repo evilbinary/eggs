781634ef78dc5dd7295d41e02aefc56e49882eb0
evilbinary <rootntsd@gmail.com>
2023-12-08 22:50:00 +0800

    add client api

--- libgaga/client.c
+++ libgaga/client.c
@@ -11,7 +11,7 @@
 
 void* client_call(client_t* client, void* fn, ...) {
   if (client == NULL) {
-    printf("client is null\n");
+    printf("call client is null\n");
     return NULL;
   }
   printf("call client %s\n", client->name);
@@ -64,7 +64,7 @@ void* client_aret(client_t* client, void* fn, ...) { return 0; }
 
 void client_run(client_t* client, client_fn fn) {
   if (client == NULL) {
-    printf("error client is null\n");
+    printf("error run client is null\n");
     return;
   }
   if (fn == NULL) {
@@ -90,7 +90,7 @@ void client_run(client_t* client, client_fn fn) {
 
 void client_run_one(client_t* client, client_fn fn) {
   if (client == NULL) {
-    printf("error client is null\n");
+    printf("error run one client is null\n");
     return;
   }
   if (fn == NULL) {
@@ -143,7 +143,6 @@ client_t* client_get(char* name) {
     return NULL;
   }
   client_t* client = malloc(sizeof(client_t));
-  printf("client get %d\n", SYS_GET_CLIENT);
 
   client_ctl_t ctl;
   client->name = name;
@@ -159,6 +158,29 @@ client_t* client_get(char* name) {
   return client;
 }
 
+client_t* client_id(int id) {
+  int fd = open("/dev/gaga", O_RDWR);
+  printf("client open fd:%d\n", fd);
+  if (fd < 0) {
+    printf("client get open %d error\n", id);
+    return NULL;
+  }
+  client_t* client = malloc(sizeof(client_t));
+
+  client_ctl_t ctl;
+  client->name = NULL;
+  ctl.id = id;
+  ctl.client = client;
+  int ret = ioctl(fd, SYS_GET_CLIENT_BY_ID, &ctl);
+  if (ret < 0) {
+    printf("client get ioctl %d error\n", id);
+    free(client);
+    return NULL;
+  }
+  printf("get client %x id %d \n",client, client->id);
+  return client;
+}
+
 void clirent_un_regist(client_t* client) {
   if (client == NULL) {
     return;
@@ -166,3 +188,4 @@ void clirent_un_regist(client_t* client) {
   int ret = close(client->fd);
   free(client);
 }
+
--- libgaga/client.h
+++ libgaga/client.h
@@ -8,31 +8,26 @@ enum {
   SYS_NEW_CLIENT = 1,
   SYS_GET_CLIENT = 2,
   SYS_DEL_CLIENT = 3,
+  SYS_GET_CLIENT_BY_ID = 4,
 };
 
-enum{
-  SYS_INIT=0,
-  SYS_RUNNING=1,
-  SYS_BLOCKING=2,
-  SYS_WAIT=3,
-  SYS_DINIT=4,
+enum {
+  SYS_INIT = 0,
+  SYS_RUNNING = 1,
+  SYS_BLOCKING = 2,
+  SYS_WAIT = 3,
+  SYS_DINIT = 4,
 };
 
-enum{
-  API_INIT =0,
-  API_REDAY=1,
-  API_RETURN=2,
-  API_FINISH=3
-};
+enum { API_INIT = 0, API_REDAY = 1, API_RETURN = 2, API_FINISH = 3 };
 
 typedef struct api {
-  int cid;        // call id
-  int state;         // api state
-  char* fn;       // function id
-  void* ret;      // return value
+  int cid;                  // call id
+  int state;                // api state
+  char* fn;                 // function id
+  void* ret;                // return value
   char args[MAX_ARGS_BUF];  // params
 } api_t;
-
 
 typedef struct client {
   char* name;
@@ -47,22 +42,22 @@ typedef struct client {
   api_t* apis;
   int state;
   int count;
-  struct client *instance[MAX_INSTANCE];
+  struct client* instance[MAX_INSTANCE];
 } client_t;
 
 typedef struct client_ctl {
   char* name;
+  int id;
   client_t* client;
-
 } client_ctl_t;
 
-typedef void* (*client_fn)(void* fn,void* args);
-
+typedef void* (*client_fn)(void* fn, void* args);
 
 client_t* client_regist(char* name);
 void clirent_un_regist(client_t* client);
 
 client_t* client_get(char* name);
+client_t* client_id(int id);
 
 void client_run_one(client_t* client, client_fn fn);
 
