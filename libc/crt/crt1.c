87a21f9522cef99e75574892cdbeb3fa66b8dcab
evilbinary <rootntsd@gmail.com>
2024-03-26 00:07:12 +0800

    ref crt1

--- libc/crt/crt1.c
+++ libc/crt/crt1.c
@@ -1,4 +1,24 @@
+#include "stdio.h"
 
-void haha_gaga(){
+__attribute__((weak)) int main();
 
+int __libc_start_main(int (*)(), int, char **,char **);
+
+typedef struct exec{
+    int argc;
+    char** argv;
+    char **envp;
+    char* filename;
+}exec_t;
+
+void start(exec_t *p) {
+  int ret = __libc_start_main(main, p->argc, p->argv,p->envp);
+  exit(ret);
+}
+
+
+int __libc_start_main(int (*main)(int, char **, char **), int argc,
+                      char **argv,char **envp) {
+  //init libc here
+  return main(argc,argv,envp);
 }
