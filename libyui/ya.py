9455bbed8e1af4c2c929b95715891947a0e6b77c
evil <rootdebug@163.com>
2026-08-01 00:51:42 +0800

    build(ya.py): add __YIYIYA__ compile definition

--- libyui/ya.py
+++ libyui/ya.py
@@ -6,11 +6,11 @@
 
 def add_flags():
     add_packages(get_config('default_libc'))
+    add_cflags('-D__YIYIYA__')
 
     pass
 add_buildin('add_flags',add_flags)
 
 includes("./lib/ya.py")
-
 # src/ya.py owns the yui static library (game/perf/input/backends).
 includes("./src/ya.py")
